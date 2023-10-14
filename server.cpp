eb37f286c29b1ebedde3aa0f97004917ce8f3d7f
gururajo <gururajotagerimail2@gmail.com>
2023-10-14 11:06:15 +0530

    had committed wrong updated committed

--- server.cpp
+++ server.cpp
@@ -1,11 +1,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "server_functions.cpp"
 #include <arpa/inet.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+using namespace std;
 
 #define PORT 8080
 #define BUF_SIZE 1024
@@ -14,7 +15,7 @@
 int main()
 {
     int sockfd, clientfd;
-    socklen_t client_size;
+    int client_size;
 
     struct sockaddr_in server_address, client_address;
 
@@ -25,14 +26,14 @@ int main()
     if (sockfd == -1)
     {
         perror("Error creating socket");
-        exit(0);
+        _exit(0);
     }
 
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
 
-    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address) == -1))
+    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
     {
         perror("Error binding socket with 8080");
         server_address.sin_port = htons(PORT + 1);
@@ -43,18 +44,16 @@ int main()
         }
         else
         {
-            printf("%d\n", 8081);
+            cout << 8081 << endl;
         }
     }
     else
-    {
-        printf("%d\n", 8080);
-    }
+        cout << 8080 << endl;
 
     if (listen(sockfd, MAX_CLIENTS) == -1)
     {
         perror("Error listening for connections");
-        exit(0);
+        _exit(0);
     }
 
     printf("Listening for connections on port ...\n");
@@ -62,18 +61,18 @@ int main()
     while (1)
     {
         client_size = sizeof(client_address);
-        clientfd = accept(sockfd, (struct sockaddr *)&client_address, &client_size);
+        clientfd = accept(sockfd, (struct sockaddr *)&client_address, (socklen_t *)&client_size);
         if (clientfd == -1)
         {
             perror("Error accepting connection");
         }
         else
         {
-            printf("Got the connection from %s:%d\n", inet_ntoa(client_address.sin_addr), client_address.sin_port);
+            cout << "Got the connection form " << inet_ntoa(client_address.sin_addr) << ":" << client_address.sin_port << endl;
             if (fork() == 0)
             {
-                printf("Created a new process\n");
-                // printf("%d\n", handle_client(clientfd, client_address));
+                cout << "Created a new process" << endl;
+                cout << handle_client(clientfd, client_address) << endl;
                 close(clientfd);
             }
             else
